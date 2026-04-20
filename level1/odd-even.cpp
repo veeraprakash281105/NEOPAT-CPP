dd5d5a56c1cea3fc62529048911d38c0d0ca5273
veeraprakash281105 <veeraprakash2811@gmail.com>
2026-04-20 12:22:41 +0545

    Create odd-even.cpp

---
+++ level1/odd-even.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(a%2!=0)
+    {
+        cout<<"The given number is odd."<<endl;
+    }
+    else
+    {
+        cout<<"The given number is even."<<endl;
+    }
+    return 0;
+}
