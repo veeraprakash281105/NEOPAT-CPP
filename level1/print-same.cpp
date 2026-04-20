b6d24f007312ccc2762d25929e0334bb4e083ece
veeraprakash281105 <veeraprakash2811@gmail.com>
2026-04-20 12:19:13 +0545

    Create print-same.cpp

---
+++ level1/print-same.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a;
+    int x;
+    float y;
+    cin>>a>>x>>y;
+    cout<<a;
+    cout<<x;
+    printf("%.2f",y);
+    return 0;
+}
